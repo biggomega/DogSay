8d125bdd48238dbeb467c4ec807c837e6c41f8ae
Ben Botvinick <benbotvinick@gmail.com>
2018-01-24 16:44:29 -0500

    Update dogsay.py

--- dogsay.py
+++ dogsay.py
@@ -12,9 +12,9 @@ def main():
     parser.add_argument('message', help='give Bruno something to say')
     args = parser.parse_args()
     print """     ______
-    / \\/ %s \____
-   /           %s
-  /       _____/ %s
+    / \\/ %s \___
+   /          %s
+  /       ____/ %s
  /_______/    %s\n""" % (args.e, args.n, args.message, args.t)
 
 

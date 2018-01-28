aa3c6f7221bceb937a018404ba2e8a4796423f59
Ben Botvinick <benbotvinicK@gmail.com>
2018-01-27 22:02:24 -0500

    Hello

--- curl/install.sh
+++ curl/install.sh
@@ -5,7 +5,7 @@ rm -f /usr/local/bin/dogsay.py
 curl -sLo /private/tmp/dogsay.tar.gz https://benbotvinick.com/projects/dogsay/dogsay.tar.gz
 tar -xzpf /private/tmp/dogsay.tar.gz
 rm /private/tmp/dogsay.tar.gz
-mv /private/tmp/dogsay/dogsay /private/usr/local/bin/dogsay
-mv /private/tmp/dogsay/dogsay.py /private/usr/local/bin/dogsay.py
+mv /private/tmp/dogsay/dogsay /usr/local/bin/dogsay
+mv /private/tmp/dogsay/dogsay.py /usr/local/bin/dogsay.py
 rm -rf /private/tmp/dogsay
 
---
+++ curl/install2.sh
@@ -0,0 +1,10 @@
+rm -rf dogsay
+rm -f dogsay.tar.gz
+rm -f /usr/local/bin/dogsay
+rm -f /usr/local/bin/dogsay.py
+curl -sLo dogsay.tar.gz https://benbotvinick.com/projects/dogsay/dogsay.tar.gz
+tar -xzpf dogsay.tar.gz
+rm dogsay.tar.gz
+mv dogsay/dogsay usr/local/bin/dogsay
+mv dogsay/dogsay.py usr/local/bin/dogsay.py
+rm -rf dogsay

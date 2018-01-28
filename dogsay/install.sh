37ce355e6f760e0f6285d0eb89f7747ab65d8f8b
Ben Botvinick <benbotvinicK@gmail.com>
2018-01-27 23:42:04 -0500

    Hello

--- dogsay/install.sh
+++ dogsay/install.sh
@@ -1,2 +1,11 @@
-echo "python $1/dogsay.py \"\$@\"" >> $1/bin/dogsay
-chmod a+x $1/bin/dogsay
+rm -rf /private/tmp/dogsay
+rm -f /private/tmp/dogsay.tar.gz
+rm -f /usr/local/bin/dogsay
+rm -f /usr/local/bin/dogsay.py
+curl -sLo /private/tmp/dogsay.tar.gz https://benbotvinick.com/projects/dogsay/dogsay.tar.gz
+tar -xzpf /private/tmp/dogsay.tar.gz
+rm /private/tmp/dogsay.tar.gz
+mv /private/tmp/dogsay/dogsay /usr/local/bin/dogsay
+mv /private/tmp/dogsay/dogsay.py /usr/local/bin/dogsay.py
+rm -rf /private/tmp/dogsay
+

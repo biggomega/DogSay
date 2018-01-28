c19449be76e7b60566a778d8043ee013ee5ff716
Ben Botvinick <benbotvinicK@gmail.com>
2018-01-27 22:16:30 -0500

    Hello

--- curl/install2.sh
+++ curl/install2.sh
@@ -5,6 +5,6 @@ rm -f /usr/local/bin/dogsay.py
 curl -sLo dogsay.tar.gz https://benbotvinick.com/projects/dogsay/dogsay.tar.gz
 tar -xzpf dogsay.tar.gz
 rm dogsay.tar.gz
-mv dogsay/dogsay usr/local/bin/dogsay
-mv dogsay/dogsay.py usr/local/bin/dogsay.py
+mv dogsay/dogsay /usr/local/bin/dogsay
+mv dogsay/dogsay.py /usr/local/bin/dogsay.py
 rm -rf dogsay
